adba785b1c6409038609697894de7ce66de0aaa6
murtazalw <murtaza_lw@yahoo.com>
2016-06-29 11:42:44 +0300

    initialize playerCounts

--- TestUserInput/src/com/example/testuserinput/Team.java
+++ TestUserInput/src/com/example/testuserinput/Team.java
@@ -3,7 +3,7 @@ package com.example.testuserinput;
 public class Team {
 	private int id;
 	private String name;
-	private int playerCounts;
+	private int playerCounts = 0;
 
 	Team(int id, String name) {
 		this.id = id;
