a9aedd67d1d8322104c634fcd53a6ebe0ef29d4b
murtazalw <murtaza_lw@yahoo.com>
2016-06-29 11:14:56 +0300

    change variable name playerCount to playerCounts

--- TestUserInput/src/com/example/testuserinput/Team.java
+++ TestUserInput/src/com/example/testuserinput/Team.java
@@ -3,7 +3,7 @@ package com.example.testuserinput;
 public class Team {
 	private int id;
 	private String name;
-	private int playerCount;
+	private int playerCounts;
 
 	Team(int id, String name) {
 		this.id = id;
